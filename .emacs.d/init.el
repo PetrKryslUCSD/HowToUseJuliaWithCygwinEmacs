94c6d301a819376d53f0f9940b56c4f8772b812f
Petr Krysl <pkrysl@ucsd.edu>
2019-11-24 16:59:58 +0000

    Updated .emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,10 +8,10 @@
              '("melpa" . "http://melpa.org/packages/") t)
 
 ;; Add our local list files to the load path.
-(add-to-list 'load-path "/home/PetrKrysl/.emacs.d/lisp")
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; Add multiple cursors
-(add-to-list 'load-path "/home/PetrKrysl/.emacs.d/lisp/multiple-cursors")
+(add-to-list 'load-path "~/.emacs.d/lisp/multiple-cursors")
 (require 'multiple-cursors)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 
@@ -24,4 +24,20 @@
 ;; When on cygwin, rewrite the file names.
 (setq julia-repl-path-rewrite-rules julia-repl-cygwin-path-rewrite-rules)
 (add-hook 'julia-mode-hook 'julia-repl-mode) ;; always use minor mode
+(setenv "JULIA_NUM_THREADS" "2")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso)))
+ '(mouse-drag-and-drop-region t)
+ '(visible-cursor t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "red")))))
