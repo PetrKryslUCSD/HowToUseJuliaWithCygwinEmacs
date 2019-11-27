9e9c5a620e3a1d8bcde8e6912e357af801298a42
Petr Krysl <pkrysl@ucsd.edu>
2019-11-27 06:14:08 +0000

    More comments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,22 +8,25 @@
 ;; Add the local list files to the load path.
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
-;; Add multiple cursors
+;; Add multiple cursors.
+;; Toggling the mode on and off, and setting the multiple cursors is
+;; carried out via voice commands.
+;; I installed the package into my local lisp directory.
 (add-to-list 'load-path "~/.emacs.d/lisp/multiple-cursors")
 (require 'multiple-cursors)
 ;; This key  will expand the selection.
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 
-;; Dynamic abbreviations
+;; Dynamic abbreviations. The lookup command is bound to a voice command.
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 
-;; Replace highlighted text with what I type?.
+;; Replace highlighted text (region) with what I type.
 (delete-selection-mode 1)
 
 ;; Show matching parentheses.
 (setq show-paren-mode t)
 
-;; Command to insert a matching pair of brackets
+;; Command to insert a matching pair of square brackets
 (global-set-key (kbd "M-[") 'insert-pair)
 
 ;; Quiet, please! No dinging!
@@ -41,13 +44,19 @@
 (add-hook 'julia-mode-hook 'julia-repl-mode) ;; always use minor mode
 (setenv "JULIA_NUM_THREADS" "2")	     ; set the default number of threads
 
-
+;; Snippets.
 ;; YASnippet is a code template system for Emacs. http://github.com/capitaomorte/yasnippet
 (yas-global-mode 1)
 
-;; Tags
+;; Tags: Exuberant CTAGS.
+;; The executable. I installed the entire directory into my home folder.
+;; Change this if it is in a different place.
 (setq path-to-ctags (expand-file-name "~/ctags58/ctags.exe")) ;; <- your ctags path here
+;; Note that the julia-ctags file is needed to inform the executable
+;; of the syntax of Julia. This one is renamed from the one available
+;; at https://github.com/JuliaEditorSupport/julia-ctags, and copied into the ctags58 installation directory.
 (setq path-to-julia-ctags (expand-file-name "~/ctags58/julia-ctags")) ;; <- your julia-ctags path here
+;; Lisp code bound to a voice command.
 (defun create-tags (dir-name)
   "Create tags file."
   (interactive "DDirectory: ")
@@ -59,30 +68,24 @@
   )
 
 ;; Git
-;; Code to be linked to a voice command.
+;; Function to create a Git shell. This code is linked to a voice command.
 (defun create-git-shell (dir-name)
   "Create git shell."
   (interactive "DDirectory: ")
   (cd dir-name)
   (eshell nil)
   (rename-buffer
-   (format "*git %s" dir-name))
+   (format "*git %s*" dir-name))
   )
 
 ;; Custom variables.
+;; I like to be able to drag regions with the mouse.
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
  '(mouse-drag-and-drop-region t)
  '(package-selected-packages (quote (markdown-mode yasnippet)))
  '(visible-cursor t))
 
+;; It is great to be able to see the cursor at the first glance.
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "red")))))
